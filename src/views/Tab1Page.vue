1fd0193a11e283f8a1a64a902927ae9596e15a47
10GO-dev <Diegoperalta0929@gmail.com>
2023-06-21 12:28:23 -0400

    first commit

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -1,23 +1,94 @@
 <template>
   <ion-page>
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
-        </ion-toolbar>
-      </ion-header>
+    <ion-list :fullscreen="true">
+    
+        <ion-item-group>
+          <ion-item-divider>
+            <ion-label class="title">Demon Slayer: (Kimetsu no Yaiba)</ion-label>
+          </ion-item-divider>
+          <ion-item lines="none">
 
-      <ExploreContainer name="Tab 1 page" />
-    </ion-content>
+            <swiper :autoplay="{ delay: 2000, disableOnInteraction: false }" :pagination="{ clickable: true }">
+              <swiper-slide>
+                <img src="https://sphinxanime.com/wp-content/uploads/2021/04/kimetsunoyaibawallpapersphinxanime.jpg" alt="kimetsu Image" />
+              </swiper-slide>
+              <swiper-slide>
+                <img src="https://www.crunchyroll.com/imgsrv/display/thumbnail/1200x675/catalog/crunchyroll/6b5f4fc8c0a7bb5012b9c0930a46570f.jpe" alt="kimetsu Image" />
+              </swiper-slide>
+              <swiper-slide>
+                <img src="https://static3.srcdn.com/wordpress/wp-content/uploads/2020/05/Demon-Slayer-Movie-2.jpg" alt="kimetsu Image" />
+              </swiper-slide>
+            </swiper>
+          </ion-item>
+          <ion-item lines="none"/>
+          <ion-item lines="none">
+            <ion-text> Es el Período Taisho en Japón. Tanjiro, un niño de buen corazón que vende carbón para ganarse la vida, encuentra a su familia asesinada por un demonio. Para empeorar las cosas, su hermana menor, Nezuko, la única superviviente, se ha transformado ella misma en un demonio.</ion-text>
+          </ion-item>
+          <ion-item lines="none"/>
+        </ion-item-group>
+        <ion-router-outlet>
+        </ion-router-outlet>
+          <a class="ion-padding" href="/tabs/tab2"><ion-icon :icon="arrowForward"></ion-icon>  Toca aquí para ver mas info.</a>
+    </ion-list>
   </ion-page>
 </template>
 
-<script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+<script lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonText, IonLabel,IonItem,IonItemGroup, IonItemDivider, IonList, IonIcon,IonRouterOutlet} from '@ionic/vue';
+import { defineComponent } from 'vue';
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import SwiperCore, { Autoplay, Pagination } from 'swiper';
+import { arrowForward } from 'ionicons/icons';
+
+// Import Swiper styles
+import 'swiper/swiper-bundle.css';
+
+// Import Ionic Swiper styles
+import '@ionic/vue/css/ionic-swiper.css';
+
+SwiperCore.use([Autoplay, Pagination]);
+
+export default defineComponent({
+  components: {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonText,
+    Swiper,
+    SwiperSlide,
+    IonLabel,
+    IonItemGroup,
+    IonItem,
+    IonItemDivider,
+    IonList,
+    IonIcon,
+    IonRouterOutlet,
+},
+ setup(){
+  return { arrowForward}
+ }
+});
 </script>
+
+<style scoped>
+.title {
+  font-size: medium;
+  
+}
+
+ion-item-divider {
+  --background: #790000cb;
+  --color: #fff;
+
+  --padding-top: 10px;
+  --padding-bottom: 10px;
+  --padding-start: 20px;
+  --padding-end: 20px;
+}
+
+ion-text{
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+</style>
--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -1,23 +1,57 @@
 <template>
   <ion-page>
+    
+    <ion-accordion-group :fullscreen="true">
     <ion-header>
-      <ion-toolbar>
-        <ion-title>Tab 2</ion-title>
-      </ion-toolbar>
+            <ion-label class="title">Demon Slayer: (Kimetsu no Yaiba)</ion-label>
     </ion-header>
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 2</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
-      <ExploreContainer name="Tab 2 page" />
-    </ion-content>
+    <ion-accordion value="first">
+      <ion-item slot="header" color="light">
+        <ion-label>First Accordion</ion-label>
+      </ion-item>
+      <div class="ion-padding" slot="content">First Content</div>
+    </ion-accordion>
+    <ion-accordion value="second">
+      <ion-item slot="header" color="light">
+        <ion-label>Second Accordion</ion-label>
+      </ion-item>
+      <div class="ion-padding" slot="content">Second Content</div>
+    </ion-accordion>
+    <ion-accordion value="third">
+      <ion-item slot="header" color="light">
+        <ion-label>Third Accordion</ion-label>
+      </ion-item>
+      <div class="ion-padding" slot="content">Third Content</div>
+    </ion-accordion>
+  </ion-accordion-group>
   </ion-page>
 </template>
 
-<script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+<script lang="ts">
+  import { IonPage, IonAccordion, IonAccordionGroup, IonItem, IonLabel,IonItemDivider } from '@ionic/vue';
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    components: {
+      IonPage,
+      IonAccordion,
+      IonAccordionGroup,
+      IonItem,
+      IonLabel,
+      IonItemDivider
+    },
+  });
 </script>
+<style scoped>
+
+ion-item-divider {
+  --background: #790000cb;
+  --color: #fff;
+  
+  --padding-top: 10px;
+  --padding-bottom: 10px;
+  --padding-start: 20px;
+  --padding-end: 20px;
+}
+
+</style>
